9cfa5df17efbbe17b0a44f752d89816cce9c6b0e
Yarix <Yarikx@gmail.com>
2013-02-20 00:54:56 +0200

    some graph works

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -1,2 +1,4 @@
 addSbtPlugin("org.ensime" % "ensime-sbt-cmd" % "0.1.0")
 
+addSbtPlugin("com.typesafe.sbteclipse" % "sbteclipse-plugin" % "2.1.1")
+
---
+++ src/main/scala/org/yarik/pzks2/Wrappers.scala
@@ -0,0 +1,8 @@
+package org.yarik.pzks2
+
+import org.jgraph.JGraph
+import scala.swing.Component
+
+class SGraph extends Component{
+  override lazy val peer = new JGraph
+}
